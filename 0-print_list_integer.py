d420920149c16b3880ac4b7b02d8a732f2555e78
ELFERDI <faradayelferdi34@gmail.com>
2024-03-17 08:26:26 -0700

    Print a list of integers

---
+++ 0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+# 0-print_list_integer.py
+
+
+def print_list_integer(my_list=[]):
+    """print all integers of a list."""
+    for i in range(lenmy_list)):
+        print("(:d)".format(my_list[i]))
+
